56ef0f98ae772f4da3723dc95e84d7ed4f1aab6d
Christopher Tse <christopher-tse@outlook.com>
2017-02-24 00:46:45 -0600

    Finalize build task

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@
 /////////////////////////////
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    prefix = require('gulp-autoprefixer'),
     plumber = require('gulp-plumber'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     gutil = require('gulp-util'),
     dedupe = require('gulp-dedupe'),
     clean = require('gulp-clean-css'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    del = require('del');
 
 /////////////////////////////
 // Scripts Task
@@ -31,8 +35,42 @@ gulp.task('sass', function() {
     .pipe(concat('styles.sass'))
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
+    .pipe(prefix({
+        browsers: ['> 5%'],
+        cascade: false
+    }))
     .pipe(clean())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({stream:true}));
+});
+
+/////////////////////////////
+// HTML Task
+/////////////////////////////
+gulp.task('html', function() {
+    gulp.src('app/**/*.html')
+    .pipe(reload({stream:true}));
+})
+
+/////////////////////////////
+// Build Tasks
+/////////////////////////////
+gulp.task('build', function() {
+    gulp.src('app/**/*.html').pipe(gulp.dest('build/'));
+    gulp.src('app/css/**/*.css').pipe(gulp.dest('build/css/'));
+    gulp.src('app/js-dist/*.jss').pipe(gulp.dest('build/js/'));
+    gulp.src('app/img/**/*').pipe(gulp.dest('build/img/'));
+});
+
+/////////////////////////////
+// browserSync Task
+/////////////////////////////
+gulp.task('browser-sync', function() {
+    browserSync({
+        server:{
+            baseDir: "./app/"
+        }
+    });
 });
 
 /////////////////////////////
@@ -41,9 +79,11 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
     gulp.watch(['app/js/**/*.js','!app/js/all.*'], ['scripts']);
     gulp.watch(['app/sass/**/*.sass'], ['sass']);
+    gulp.watch(['app/**/*.html'], ['html']);
+
 });
 
 /////////////////////////////
 // Default Task
 /////////////////////////////
-gulp.task('default', ['scripts','watch']);
+gulp.task('default', ['scripts','sass','html','browser-sync','watch']);
